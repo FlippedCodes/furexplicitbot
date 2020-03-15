952e1e248b15d6f7339328941b77b7ba6707e4a1
Phil <philip.schaefer0@gmail.com>
2020-03-15 11:10:15 +0100

    deleted test command

--- functions/e621_core_rewrite.js
+++
@@ -1,9 +0,0 @@
-const rp = require('request-promise');
-
-module.exports.run = async (client, message, args, config, RichEmbed, messageOwner, fa_token_A, fa_token_B) => {
-
-};
-
-module.exports.help = {
-  name: 'e621_core_rewrite',
-};
