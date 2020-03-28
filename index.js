e0c91d8d649fe5aeaa18d239ec65e79b0e15bf3f
Phil <philip.schaefer0@gmail.com>
2020-03-28 12:29:50 +0100

    moved code to own files

--- index.js
+++ index.js
@@ -22,76 +22,14 @@ function timeout(id) {
   // 5sec timeout
 }
 
-// login
-let token;
-let clientID;
-let fa_token_A;
-let fa_token_B;
-let DB;
-if (fs.existsSync('./config/test_token.json')) {
-  token = require('./config/test_token.json');
-  client.login(token.test_token_fa);
-  clientID = config.clientIDTesting;
-  fa_token_A = token.fa_cookie_a;
-  fa_token_B = token.fa_cookie_b;
-  // DB = mysql.createConnection({
-  //   host: token.DB_host,
-  //   user: token.DB_user,
-  //   password: token.DB_passw,
-  //   database: token.DB_name,
-  // });
-} else {
-  client.login(process.env.BOT_TOKEN_FA);
-  clientID = config.clientID;
-  fa_token_A = process.env.FA_COOKIE_A;
-  fa_token_B = process.env.FA_COOKIE_B;
-  // DB = mysql.createConnection({
-  //   host: process.env.DB_host,
-  //   user: process.env.DB_user,
-  //   password: process.env.DB_passw,
-  //   database: process.env.DB_name,
-  // });
-}
-
-client.commands = new Discord.Collection();
-fs.readdir('./commands/', (err, files) => {
-  if (err) console.error(err);
-
-  const jsfiles = files.filter((f) => f.split('.').pop() === 'js');
-  if (jsfiles.length <= 0) {
-    console.log('No CMD(s) to load!');
-    return;
-  }
-
-  console.log(`Loading ${jsfiles.length} command(s)...`);
-  jsfiles.forEach((f, i) => {
-    const probs = require(`./commands/${f}`);
-    console.log(`    ${i + 1}) Loaded: ${f}!`);
-    client.commands.set(probs.help.name, probs);
-  });
-  console.log(`Loaded ${jsfiles.length} command(s)!`);
+// import Functions and Commands
+config.setup.startupFunctions.forEach((FCN) => {
+  const INIT = require(`./functions/${FCN}.js`);
+  INIT.run(client, fs, config);
 });
 
-// ---------------------
-
-client.functions = new Discord.Collection();
-fs.readdir('./functions/', (err, files) => {
-  if (err) console.error(err);
-
-  const jsfiles = files.filter((f) => f.split('.').pop() === 'js');
-  if (jsfiles.length <= 0) {
-    console.log('No function(s) to load!');
-    return;
-  }
-
-  console.log(`Loading ${jsfiles.length} function(s)...`);
-  jsfiles.forEach((f, i) => {
-    const probs = require(`./functions/${f}`);
-    console.log(`    ${i + 1}) Loaded: ${f}!`);
-    client.functions.set(probs.help.name, probs);
-  });
-  console.log(`Loaded ${jsfiles.length} function(s)!`);
-});
+// create conenction to DB
+require('./database/SETUP_DBConnection');
 
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag} serving ${client.guilds.size} Servers!`);
