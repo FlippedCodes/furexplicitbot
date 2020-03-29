1bab1c1b50a30ab00152860c49eff80c762c7bc8
Phil <philip.schaefer0@gmail.com>
2020-03-29 01:02:40 +0100

    updated db

--- migrations/20200328115824-seenChangelogs.js
+++ migrations/20200328115824-seenChangelogs.js
@@ -3,7 +3,10 @@ module.exports = {
     userID: {
       type: Sequelize.STRING(30),
       allowNull: false,
+      primaryKey: true,
+      unique: true,
     },
-  }),
+  },
+  { timestamps: false }),
   down: (queryInterface, Sequelize) => queryInterface.dropTable('seenChangelogs'),
 };
