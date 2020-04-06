3d948ca86a45d72c7318cd7bd4e709a897dcc6d1
Phil <philip.schaefer0@gmail.com>
2020-04-06 22:11:46 +0200

    inital init

---
+++ functions/disabled/FUNC_getOwnPrefix_org.js
@@ -0,0 +1,29 @@
+const prefixCache = new Map();
+
+const ServerPrefix = require('../../database/models/ServerPrefix');
+
+const config = require('../../config/main.json');
+
+const errHander = (err) => {
+  console.error('ERROR:', err);
+};
+
+function cacheHandler(serverID, prefix) {
+  prefixCache.set(serverID, prefix);
+  setTimeout(() => prefixCache.delete(serverID).catch(), 600000);
+}
+
+module.exports.run = async (serverID) => {
+  if (prefixCache.has(serverID)) return prefixCache.get(serverID);
+  const DBEentry = await ServerPrefix.findOne({ where: { serverID } }).catch(errHander);
+  if (DBEentry) {
+    const prefix = DBEentry.prefix;
+    cacheHandler(serverID, prefix);
+    return prefix;
+  }
+  return config.prefix.default;
+};
+
+module.exports.help = {
+  name: 'FUNC_getOwnPrefix',
+};
