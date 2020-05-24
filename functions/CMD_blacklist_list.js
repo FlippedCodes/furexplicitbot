0119767516cee3aa651c45fcfe24c10a8c1a96fe
Phil <philip.schaefer0@gmail.com>
2020-05-24 18:30:53 +0200

    inital init

---
+++ functions/CMD_blacklist_list.js
@@ -0,0 +1,8 @@
+// DISABLED: not done
+module.exports.run = async (client, message, args, config, RichEmbed) => {
+  message.reply('lst');
+};
+
+module.exports.help = {
+  name: 'CMD_blacklist_list',
+};
---
+++ functions/CMD_blacklist_remove.js
@@ -0,0 +1,8 @@
+// DISABLED: not done
+module.exports.run = async (client, message, args, config) => {
+  message.reply('rm');
+};
+
+module.exports.help = {
+  name: 'CMD_blacklist_remove',
+};
