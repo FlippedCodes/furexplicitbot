5f5ea947a6abb656dfb80ca2e1e6a25111b38152
Phil <philip.schaefer0@gmail.com>
2020-05-24 19:04:10 +0200

    populated code

--- functions/CMD_blacklist_list.js
+++ functions/CMD_blacklist_list.js
@@ -1,6 +1,23 @@
-// DISABLED: not done
+const config = require('../config/main.json');
+
+const servertagsblacklist = require('../database/models/servertagsblacklist');
+
+async function getTags(serverID) {
+  const result = await servertagsblacklist.findAll({ attributes: ['tag'], where: { serverID: [serverID, config.managementServerID] } });
+  return result;
+}
+
 module.exports.run = async (client, message, args, config, RichEmbed) => {
-  message.reply('lst');
+  const DBentries = await getTags(message.guild.id);
+  const blacklistedTags = [];
+  DBentries.forEach((entry) => {
+    blacklistedTags.push(entry.tag);
+  });
+  const embed = new RichEmbed()
+    .setColor(message.member.displayColor)
+    .setAuthor('Blacklisted tagws in thwis serwer:')
+    .setDescription(`\`\`\`${blacklistedTags.join(', ')}\`\`\``);
+  message.channel.send({ embed });
 };
 
 module.exports.help = {
