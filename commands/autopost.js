b8a713665724f98b1e88e1ecfbd30f791d3648c6
Phil <philip.schaefer0@gmail.com>
2020-06-01 10:37:33 +0200

    inital init

---
+++ commands/autopost.js
@@ -0,0 +1,9 @@
+module.exports.run = async (client, message, args, config, RichEmbed, messageOwner) => {
+
+};
+
+module.exports.help = {
+  name: 'autopost',
+  title: 'Auto-Post e621',
+  desc: 'Auto Post e621 pictures in a channel.',
+};
