89d32f2dd3c8d1422e7bdef3992ef2d8e86e21fd
Phil <philip.schaefer0@gmail.com>
2020-06-02 00:00:52 +0200

    code copy and rewrite

--- functions/CMD_autopost_add.js
+++ functions/CMD_autopost_add.js
@@ -17,29 +17,50 @@ function messageFail(message, body) {
     .then((msg) => msg.delete(10000));
 }
 
-async function addTag(tag, serverID, managementServerID) {
+async function addAutopost(tag, serverID, managementServerID) {
   if (await servertagsblacklist.findOne({ where: { serverID: [serverID, managementServerID], tag } }).catch(errHander)) return false;
   await servertagsblacklist.findOrCreate({ where: { serverID, tag } }).catch(errHander);
   return true;
 }
 
+function tagsReplace(tags, search, replace) {
+  return tags.replace(new RegExp(search, 'g'), replace);
+}
+
+async function getTags(message, tags) {
+  tags = tagsReplace(tags, ', ', ' ');
+  const safeTags = await message.client.functions.get('FUNC_tagsCleanup').run(message, tags);
+  return safeTags;
+}
+
 module.exports.run = async (client, message, args, config, RichEmbed, prefix) => {
+  // TODO: check if max allowed channels is hit
+  // TODO: check if interval minisecs is to low/high
   // check if user can manage servers
   if (!message.member.hasPermission('MANAGE_GUILD')) return messageFail(message, 'You dwon\'t hawe access to thwis command òwó');
-  const [subcmd, tag] = args;
-  if (!tag) {
+  const [subcmd, interval, tagCheck] = args;
+  if (!tagCheck) {
     return messageFail(message,
       `Command usage: 
-      \`\`\`${prefix}${module.exports.help.parent} ${subcmd} TAGNAME\`\`\``);
+      \`\`\`${prefix}${module.exports.help.parent} ${subcmd} INTERVALMILLISECONDS TAGNAME\`\`\``);
   }
-  if (tag.length > 30) {
-    return messageFail(message, 'Your tag is too long. The maximum length is 30 characters.');
+  const tags = await getTags(message, args.join(' ').slice(subcmd.length + 1 + interval.length + 1));
+  if (tags.length > 255) {
+    return messageFail(message, 'Your tags are too long. The maximum length is 255 characters, minus the blacklisted tags in this server.');
   }
-  const added = await addTag(tag, message.guild.id, config.managementServerID);
-  if (added) {
-    messageSuccess(message, `\`${tag}\` has been added to the serwers blacklist.`);
-  } else {
-    messageFail(message, `\`${tag}\` is already added to thwis serwers backlist.`);
+  const added = await addAutopost(tags, message.guild.id, config.managementServerID);
+  switch (added) {
+    case true:
+      messageSuccess(message, `Your autopost with the \`${tags}\` has been created.`);
+      return;
+    case 1:
+      messageFail(message, 'You already have 2 autopost channels in this server!');
+      return;
+    case 2:
+      messageFail(message, 'You are already using this channel as an autopost channel!');
+      return;
+    default:
+      break;
   }
 };
 
