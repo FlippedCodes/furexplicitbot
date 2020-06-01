641e6373a20c39622d7f613a23b6f5d3e02c47bc
Phil <philip.schaefer0@gmail.com>
2020-06-01 22:52:47 +0200

    populated with code

--- functions/TCKR_autopost.js
+++ functions/TCKR_autopost.js
@@ -1,5 +1,45 @@
-module.exports.run = async (message) => {
+const { RichEmbed } = require('discord.js');
 
+const { Op } = require('sequelize');
+
+const autopostchannel = require('../database/models/autopostchannel');
+
+async function getChannels(currentTimestamp, config) {
+  // currentTimestamp += config.e621.autopost.dbOffset;
+  const result = await autopostchannel.findAll({ where: { nextEvent: { [Op.lt]: currentTimestamp } } });
+  return result;
+}
+
+function updateTime(channelID, currentTimestamp, interval) {
+  const nextEvent = Number(currentTimestamp) + Number(interval);
+  autopostchannel.update({ nextEvent }, { where: { channelID } });
+}
+
+function postMessage(post, channel, config) {
+  const embed = new RichEmbed();
+  embed
+    .setColor(config.e621.color)
+    .setTitle(`Artist: ${post.artist} [e621 link]`)
+    .setURL(`https://e621.net/posts/${post.postID}`)
+    .setImage(post.directLink)
+    .setFooter(config.e621.label, config.e621.logo)
+    .setTimestamp();
+  channel.send({ embed });
+}
+
+module.exports.run = (client, config) => {
+  setInterval(async () => {
+    const date = new Date();
+    const currentTimestamp = date.getTime();
+    const channels = await getChannels(currentTimestamp, config);
+    channels.forEach(async (autoPost) => {
+      const channelID = autoPost.channelID;
+      const channel = client.channels.find((channel) => channel.id === channelID);
+      const post = await client.functions.get('FUNC_autopostGetPictures').run(autoPost.tags, channelID, channel.nsfw);
+      postMessage(post, channel, config);
+      updateTime(channelID, currentTimestamp, autoPost.interval);
+    });
+  }, config.e621.autopost.intervalChecker);
 };
 
 module.exports.help = {
