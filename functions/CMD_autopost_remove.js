385e137d8024987fd474d06f29bb9dff6fa965fd
Phil <philip.schaefer0@gmail.com>
2020-06-02 21:35:03 +0200

    updated code to match db standard

--- functions/CMD_autopost_remove.js
+++ functions/CMD_autopost_remove.js
@@ -1,4 +1,6 @@
-const servertagsblacklist = require('../database/models/servertagsblacklist');
+const autopostchannel = require('../database/models/autopostchannel');
+
+const postcache = require('../database/models/postcache');
 
 const errHander = (err) => { console.error('ERROR:', err); };
 
@@ -17,26 +19,21 @@ function messageFail(message, body) {
     .then((msg) => msg.delete(10000));
 }
 
-async function removeTag(tag, serverID) {
-  if (!await servertagsblacklist.findOne({ where: { serverID, tag } }).catch(errHander)) return false;
-  await servertagsblacklist.destroy({ where: { serverID, tag } }).catch(errHander);
+async function removeAutopost(channelID) {
+  if (!await autopostchannel.findOne({ where: { channelID } }).catch(errHander)) return false;
+  await postcache.destroy({ where: { channelID } }).catch(errHander);
+  await autopostchannel.destroy({ where: { channelID } }).catch(errHander);
   return true;
 }
 
 module.exports.run = async (client, message, args, config, RichEmbed, prefix) => {
   // check if user can manage servers
   if (!message.member.hasPermission('MANAGE_GUILD')) return messageFail(message, 'You dwon\'t hawe access to thwis command òwó');
-  const [subcmd, tag] = args;
-  if (!tag) {
-    return messageFail(message,
-      `Command usage: 
-      \`\`\`${prefix}${module.exports.help.parent} ${subcmd} TAGNAME\`\`\``);
-  }
-  const added = await removeTag(tag, message.guild.id);
+  const added = await removeAutopost(message.channel.id);
   if (added) {
-    messageSuccess(message, `\`${tag}\` has been removed from the serwers blacklist.`);
+    messageSuccess(message, 'This channel does now no longer autopost.');
   } else {
-    messageFail(message, `\`${tag}\` doesn't exist on the serwers backlist. \n(Keep in mind that we have also globally blocked tags!)`);
+    messageFail(message, 'This channel doesn\'t have an autopost configured!');
   }
 };
 
