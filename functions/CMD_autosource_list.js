16b5d18d53648b81e92252e9c97e43de7c10c6de
Phil <philip.schaefer0@gmail.com>
2020-06-03 23:01:39 +0200

    inital init

---
+++ functions/CMD_autosource_list.js
@@ -0,0 +1,26 @@
+const config = require('../config/main.json');
+
+const autosourcechannel = require('../database/models/autosourcechannel');
+
+async function getCheck(serverID) {
+  const result = await autosourcechannel.findAll({ attributes: ['channelID'], where: { serverID } });
+  return result;
+}
+
+module.exports.run = async (client, message, args, config, RichEmbed, prefix) => {
+  const DBentries = await getCheck(message.guild.id);
+  const enabledChannels = [];
+  DBentries.forEach((entry) => {
+    enabledChannels.push(entry.channelID);
+  });
+  const embed = new RichEmbed()
+    .setColor(message.member.displayColor)
+    .setAuthor('Auto source channels in thwis serwer:')
+    .setDescription(`<#${enabledChannels.join('>, <#')}>`);
+  message.channel.send({ embed });
+};
+
+module.exports.help = {
+  name: 'CMD_autosource_list',
+  parent: 'autosource',
+};
---
+++ functions/CMD_autosource_remove.js
@@ -0,0 +1,41 @@
+const autosourcechannel = require('../database/models/autosourcechannel');
+
+const errHander = (err) => { console.error('ERROR:', err); };
+
+// creates a embed messagetemplate for succeded actions
+function messageSuccess(message, body) {
+  const client = message.client;
+  client.functions.get('FUNC_richEmbedMessage')
+    .run(client.user, message.channel, body, '', 4296754, false);
+}
+
+// creates a embed messagetemplate for failed actions
+function messageFail(message, body) {
+  const client = message.client;
+  client.functions.get('FUNC_richEmbedMessage')
+    .run(client.user, message.channel, body, '', 16449540, false)
+    .then((msg) => msg.delete(10000));
+}
+
+async function removeCheck(channelID) {
+  if (!await autosourcechannel.findOne({ where: { channelID } }).catch(errHander)) return false;
+  await autosourcechannel.destroy({ where: { channelID } }).catch(errHander);
+  return true;
+}
+
+module.exports.run = async (client, message, args, config, RichEmbed, prefix) => {
+  // check if user can manage servers
+  if (!message.member.hasPermission('MANAGE_GUILD')) return messageFail(message, 'You dwon\'t hawe access to thwis command òwó');
+  const channelID = message.channel.id;
+  const added = await removeCheck(channelID);
+  if (added) {
+    messageSuccess(message, `<#${channelID}> has been remowed from the auto checking liwst.`);
+  } else {
+    messageFail(message, `<#${channelID}> is nowt on the auto checking liwst.`);
+  }
+};
+
+module.exports.help = {
+  name: 'CMD_autosource_remove',
+  parent: 'autosource',
+};
