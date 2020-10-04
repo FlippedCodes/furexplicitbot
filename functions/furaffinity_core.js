23e426e1839e6ca5d1cb39374fb4e89a198dfbfa
Phil <philip.schaefer0@gmail.com>
2020-10-04 20:08:19 +0200

    updated code to v12

--- functions/furaffinity_core.js
+++ functions/furaffinity_core.js
@@ -10,7 +10,7 @@ function Timeout(msg, userID, messageOwner, config) {
   messageOwner.set(msg.id, userID);
   setTimeout(() => {
     messageOwner.delete(msg.id);
-    msg.clearReactions().catch();
+    msg.reactions.removeAll().catch();
   }, config.reactionsTimeout);
 }
 
