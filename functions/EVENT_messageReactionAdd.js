d180c1710895be2b1371fd147c8b9bd0d8f99976
Phil <philip.schaefer0@gmail.com>
2021-01-13 22:04:36 +0100

    added other emojis to switcher

--- functions/EVENT_messageReactionAdd.js
+++ functions/EVENT_messageReactionAdd.js
@@ -38,6 +38,9 @@ module.exports.run = async (client, reaction, user, config, MessageEmbed, messag
       // reaction selector
       switch (reaction.emoji.name) {
         case 'all_details': return client.functions.get('FUNC_e621_detailed').run(reaction, config, MessageEmbed);
+        case '◀️': return client.functions.get('FUNC_e621_poolBack').run(reaction, config, MessageEmbed);
+        case '🔢': return client.functions.get('FUNC_e621_poolOverview').run(reaction, config, MessageEmbed);
+        case '▶️': return client.functions.get('FUNC_e621_poolFore').run(reaction, config, MessageEmbed);
         default: return;
       }
     }
