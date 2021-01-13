e9c04d2186dee0cba828ff33609f79533bda6d86
Phil <philip.schaefer0@gmail.com>
2021-01-13 21:19:12 +0100

    inital init

---
+++ functions/FUNC_e621_pool_getData.js
@@ -0,0 +1,9 @@
+module.exports.run = async (RichEmbed) => {
+  // check if pool is present (isPool)
+  // get current page (currentPage)
+  // return list of all posts (array: all posts)
+};
+
+module.exports.help = {
+  name: 'FUNC_e621_pool_getData',
+};
