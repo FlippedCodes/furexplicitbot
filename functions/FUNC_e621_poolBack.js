b50c054bd0b99f7445ae48fbff31c51343ace0a8
Phil <philip.schaefer0@gmail.com>
2021-01-16 19:06:19 +0100

    code cleanup and message preperation

--- functions/FUNC_e621_poolBack.js
+++ functions/FUNC_e621_poolBack.js
@@ -1,51 +1,6 @@
-// creates a embed messagetemplate for failed actions
-function messageFail(message, body) {
-  const client = message.client;
-  client.functions.get('FUNC_richEmbedMessage')
-    .run(client.user, message.channel, body, '', 16449540, false)
-    .then((msg) => msg.delete({ timeout: 10000 }));
-}
-
-function buildRequest(id, config, type) {
-  const version = require('../package.json');
-  return {
-    method: 'GET',
-    uri: `https://e621.net/${type}/${id}.json?login=${config.env.get('e621_login')}&api_key=${config.env.get('e621_api_key')}`,
-    headers: {
-      'User-Agent': `FurExplicitBot/${version.version} by Flipper on e621`,
-    },
-    json: true,
-  };
-}
-
-async function getRequest(request) {
-  const rp = require('request-promise');
-  const result = await rp(request);
-  return result;
-}
-
-async function requestPicture(id, config) {
-  const post = await getRequest(buildRequest(id, config, 'posts'));
-  return post.post;
-}
-
-async function requestPool(id, config) {
-  const pool = await getRequest(buildRequest(id, config, 'pools'));
-  return pool;
-}
-
-function formatTags(tags) {
-  const joinedTags = tags.join(', ');
-  return `\`${joinedTags}\``;
-}
-
 function postPicture(reaction, RichEmbed, previewMessage, config, post, poolData) {
   const embed = new RichEmbed();
 
-  if (post.pools.length) {
-    embed.addField('Pool', `https://e621.net/pools/${poolData.id}`, true);
-    embed.addField('Pool Index', poolData.post_ids.indexOf(post.id), true);
-  }
   const extention = post.file.ext;
   if (extention === 'webm' || extention === 'swf') {
     embed.addField('Direct video link', post.file_url);
@@ -61,25 +16,15 @@ function postPicture(reaction, RichEmbed, previewMessage, config, post, poolData
   embed
     .setColor(previewMessage.color)
     .setTitle('E621 Link')
-    .setURL(`https://e621.net/posts/${post.id}`)
-    .setDescription(`**Tags:** \`\`\`${post.tags.general.join(', ')}\`\`\``)
-    .addField('Rating', post.rating, true)
-    .addField('Score', post.score.total, true)
-    .addField('ID', post.id, true)
-    .addField('Resolution', `${post.file.width}x${post.file.height}`, true)
-    .addField(typeSources, source)
-    .addField('Full Picture link', post.file.url)
-    .setImage(post.file.url)
+    .setURL(`https://e621.net/posts/${ID}`)
+    .addField('Pool', `https://e621.net/pools/${POOLLINK}`, true)
+    .addField('Pool Index', poolData.post_ids.indexOf(POOLINDEX), true)
+    .addField('Pool laste Page', poolData.post_ids.length, true)
+    .addField('Full Picture link', URL)
+    .setImage(URL)
     .setFooter(config.e621.label, config.e621.logo)
     .setTimestamp();
   reaction.message.edit({ embed });
-}
-
-async function postReactions(reaction, config, post) {
-  const pool = await requestPool(post.pools[0], config);
-  if (post.id !== pool.post_ids.front) await reaction.message.react('◀️');
-  await reaction.message.react('🔢');
-  if (post.id !== pool.post_ids.back) await reaction.message.react('▶️');
 }
 
 module.exports.run = async (reaction, config, RichEmbed) => {
