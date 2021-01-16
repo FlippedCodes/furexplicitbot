2d94457f3a2fd49fd652b3cd88037cca992caa48
Phil <philip.schaefer0@gmail.com>
2021-01-16 21:43:30 +0100

    deprecated

--- functions/FUNC_e621_poolGetData.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (message) => {
-  // check if pool is present (isPool)
-  // get current page (currentPage)
-  // return list of all posts (array: all posts)
-};
-
-module.exports.help = {
-  name: 'FUNC_e621_poolGetData',
-};
