ea522178c80abebc5d85ab2012ca2926d67c5e70
Phil <philip.schaefer0@gmail.com>
2022-01-14 20:29:40 +0100

    envorced codestyle

--- database/models/autosourcechannel.js
+++ database/models/autosourcechannel.js
@@ -1,20 +1,23 @@
 /* eslint-disable no-undef */
 const Sequelize = require('sequelize');
 
-module.exports = sequelize.define('autosourcechannel', {
-  channelID: {
-    type: Sequelize.STRING(30),
-    primaryKey: true,
-  },
-  serverID: {
-    type: Sequelize.STRING(30),
-    allowNull: false,
-  },
-},
-{
-  uniqueKeys: {
-    uniqueAutosource: {
-      fields: ['serverID', 'channelID'],
+module.exports = sequelize.define(
+  'autosourcechannel',
+  {
+    channelID: {
+      type: Sequelize.STRING(30),
+      primaryKey: true,
+    },
+    serverID: {
+      type: Sequelize.STRING(30),
+      allowNull: false,
     },
   },
-});
+  {
+    uniqueKeys: {
+      uniqueAutosource: {
+        fields: ['serverID', 'channelID'],
+      },
+    },
+  },
+);
