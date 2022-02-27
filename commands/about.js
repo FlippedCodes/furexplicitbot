538183e375e9d6a9ccaca7aa07bd3e6ab53a5eb6
Phil <philip.schaefer0@gmail.com>
2022-02-27 19:15:18 +0100

    [update] [moved] bot description

--- commands/about.js
+++ commands/about.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports.run = async (interaction) => {
-  fs.readFile('./config/about.txt', 'utf8', (err, content) => {
+  fs.readFile('./about.txt', 'utf8', (err, content) => {
     if (err) {
       console.log(err);
       messageFail(interaction, uwu('Oh, no! Something went wrong. Sorry about that :('));
