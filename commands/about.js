a7facad5ce651617da0646a460fac4a6421da13b
Phil <philip.schaefer0@gmail.com>
2022-02-27 19:42:55 +0100

    [cleanup]

--- commands/about.js
+++ commands/about.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 
 module.exports.run = async (interaction) => {
-  fs.readFile('./about.txt', 'utf8', (err, content) => {
+  fs.readFile(config.commands.about.text, 'utf8', (err, content) => {
     if (err) {
       console.log(err);
       messageFail(interaction, uwu('Oh, no! Something went wrong. Sorry about that :('));
       return;
     }
-    messageSuccess(interaction, uwu(content));
+    messageSuccess(interaction, uwu(content), 0xFAAF3A, true);
   });
 };
 
