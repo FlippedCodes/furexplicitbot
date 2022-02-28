6f13c832f9dbe7fe24da67efe9252516ba2a90d9
Phil <philip.schaefer0@gmail.com>
2022-02-28 18:45:45 +0100

    [removed] commands no longer needed/upgraded

--- commands_old/alias_ibunny.js
+++
@@ -1,8 +0,0 @@
-module.exports.run = async (client, message, args, config, MessageEmbed, messageOwner) => {
-  client.functions.get('FUNC_inkbunny_core').run(client, message, args, config, MessageEmbed, messageOwner)
-    .catch(console.log);
-};
-
-module.exports.help = {
-  name: 'ibunny',
-};
