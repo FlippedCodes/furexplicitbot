5c78f1272aa6347fc3d2fc752584a9aa89ff45d1
Phil <philip.schaefer0@gmail.com>
2022-06-12 03:20:50 +0200

    [add] note

--- commands/blacklist/remove.js
+++ commands/blacklist/remove.js
@@ -14,6 +14,7 @@ function pruneAutopost(channelID) {
 module.exports.run = async (interaction, servertagsblacklist, tag) => {
   const added = await removeTag(servertagsblacklist, tag, interaction.guild.id);
   if (added) {
+    // FIXME: LOW: this is a hack to not show tag name in the success and error message. can be fixed by setting the value to the same as the on screen name
     messageSuccess(interaction, 'Tag has been removed from the serwers blacklist.');
     pruneAutopost(interaction.channel.id);
   } else {
