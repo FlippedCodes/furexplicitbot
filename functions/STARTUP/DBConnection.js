a6b0e88a32b8718538334dcdeccc62fecbb00fa6
Phil <philip.schaefer0@gmail.com>
2022-07-11 17:38:34 +0200

    [fix] env names

--- functions/STARTUP/DBConnection.js
+++ functions/STARTUP/DBConnection.js
@@ -4,12 +4,12 @@ module.exports.run = async () => {
   LOG('[DB] Connecting...');
 
   const sequelize = await new Sequelize(
-    process.env.DBdatabase,
-    process.env.DBusername,
-    process.env.DBpassword,
+    process.env.db_name,
+    process.env.db_user,
+    process.env.db_passw,
     {
-      host: process.env.DBhost,
-      dialect: 'mysql',
+      host: process.env.db_host,
+      dialect: process.env.db_dialect,
       logging: DEBUG ? console.log() : DEBUG,
     },
   );
