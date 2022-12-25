fdb7c5918cd3e88888fc79f82f9b8ab8d70555e0
Phil <philip.schaefer0@gmail.com>
2022-12-25 05:22:59 +0100

    [update] id is needed as channelid can be provided multiple times

--- database/models/autopostfasubmission.js
+++ database/models/autopostfasubmission.js
@@ -4,14 +4,19 @@ const Sequelize = require('sequelize');
 module.exports = sequelize.define(
   'autopostfasubmission',
   {
+    ID: {
+      type: Sequelize.INTEGER(10),
+      primaryKey: true,
+      autoIncrement: true,
+    },
     channelID: {
       type: Sequelize.STRING(30),
-      primaryKey: true,
+      allowNull: false,
     },
     serverID: {
       type: Sequelize.STRING(30),
       allowNull: false,
     },
-    artistName: Sequelize.TEXT('tiny'),
+    artistID: Sequelize.TEXT('tiny'),
   },
 );
