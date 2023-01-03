fe19267df31fd62cf72ce4363f3425942fe71ec1
Phil <philip.schaefer0@gmail.com>
2023-01-03 17:25:57 +0100

    [update] added author check and watch them

--- commands/faautopost/add.js
+++ commands/faautopost/add.js
@@ -1,3 +1,5 @@
+const { Author } = require('furaffinity-api');
+
 async function countChannels(autopostfasubmission, serverID) {
   const result = await autopostfasubmission.findAndCountAll({ where: { serverID } }).catch(ERR);
   return result.count;
@@ -15,7 +17,15 @@ module.exports.run = async (interaction, autopostfasubmission) => {
   const channel = await interaction.options.getChannel('channel');
   const artistID = await interaction.options.getString('artistid');
   if (!channel.nsfw) return messageFail(interaction, uwu('Please make sure your channels is marked as ßßage-restricted!'));
-  // TODO: Verify artistid and watch artist for updates
+  // get artist catch, if not there
+  const artist = await Author(artistID).catch(() => {
+    messageFail(interaction, uwu(`The artist ßß${artistID} doesn't exist.\nPlease make sure you use the name from the URL while being on the artists page.`));
+    return;
+  });
+  // check if artist is null
+  if (!artist) return;
+  // if artist is not already watched by another channel, do so
+  if (!artist.stats.watching) artist.watchAuthor();
   const added = await addAutopost(autopostfasubmission, artistID, channel.id, interaction.guild.id, config.commands.faAutopost.maxArtists);
   switch (added) {
     case true: return messageSuccess(interaction, uwu(`You are now watching ßß\`${artistID}\` in ${channel}. The first post will appear with the next uplaod of the artist.`));
