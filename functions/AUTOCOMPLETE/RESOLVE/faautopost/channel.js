f33373dabfb6bbbb0dc2deb65a6d4dd68f837fcc
Phil <philip.schaefer0@gmail.com>
2023-04-04 21:46:51 +0200

    update - issue with over 10 results

--- functions/AUTOCOMPLETE/RESOLVE/faautopost/channel.js
+++ functions/AUTOCOMPLETE/RESOLVE/faautopost/channel.js
@@ -1,12 +1,9 @@
+const { Op } = require('sequelize');
+
 const autopostfasubmission = require('../../../../database/models/autopostfasubmission');
 
-async function getChannels(serverID) {
-  const result = await autopostfasubmission.findAll({ where: { serverID } });
-  return result;
-}
-
 module.exports.run = async (searchInput, serverID) => {
-  const DBentries = await getChannels(serverID);
+  const DBentries = await autopostfasubmission.findAll({ where: { serverID, artistID: { [Op.substring]: searchInput } }, limit: 25 });
 
   const output = DBentries.map((entry) => {
     const channel = client.channels.cache.find((channel) => channel.id === entry.channelID);
