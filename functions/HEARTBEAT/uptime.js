b8730c018574e4a4e1c5f0173382fc876d23902d
Phil <philip.schaefer0@gmail.com>
2023-12-23 16:23:21 +0100

    shardID is not there in time. fix -1 array issue

--- functions/HEARTBEAT/uptime.js
+++ functions/HEARTBEAT/uptime.js
@@ -8,14 +8,12 @@ const params = (pingRaw) => (
   }
 );
 
-function sendHeartbeat(uptimeToken) {
-  axios.get(`${config.functions.heartbeat.uptime.endpoint}${uptimeToken}`, { params: params(client.ws.ping) });
+function sendHeartbeat() {
+  // env token_uptime is a json array: "['token1', 'token2', ...]"
+  axios.get(`${config.functions.heartbeat.uptime.endpoint}${JSON.parse(process.env.token_uptime)[currentShardID]}`, { params: params(client.ws.ping) });
 }
 
 module.exports.run = async () => {
-  // input is a json array: "['token1', 'token2', ...]"
-  const uptimeToken = JSON.parse(process.env.token_uptime)[currentShardID];
-  if (!uptimeToken) return LOG('No Uptime code available for my shard!');
   setInterval(() => {
     sendHeartbeat(uptimeToken);
   }, config.functions.heartbeat.uptime.interval * 1000);
