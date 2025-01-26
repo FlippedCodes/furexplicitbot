cc4db965812258ed0837db8e3851abb531e293c1
Phil <philip.schaefer0@gmail.com>
2025-01-26 22:21:11 +0100

    remove delay + increase timeout

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const { ShardingManager } = require('discord.js');
 const manager = new ShardingManager('./bot.js', { token: process.env.token_discord });
 
 manager.spawn({
-  delay: 10 * 1000,
-  timeout: 60 * 1000,
+  // delay: 10 * 1000,
+  timeout: 120 * 1000,
 });
 
 manager.on('shardCreate', (shard) => {
